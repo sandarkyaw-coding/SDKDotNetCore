c6d3189b8c57661fdfbdbf1d3ed96f856447ff85
dardar <dars7703@gmail.com>
2024-04-14 17:23:50 +0630

    [Complete] Ado.Net CRUD

---
+++ README.md
@@ -0,0 +1,13 @@
+suggestion => crtl + . 
+
+break point => F9
+for each line => F10
+for details => F11
+
+connection -> default value = null 
+
+dataset => dataTable
+dataTable => dataRow
+dataRow => dataColumn
+
+parameter = @ sign => insert value
--- SDKDotNetCore.ConsoleApp/Program.cs
+++ SDKDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SDKDotNetCore.ConsoleApp;
+
 Console.WriteLine("Hello, World!");
+
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("C#", "Dardar", "C# Learning");
+//adoDotNetExample.Update(1002, "C# Update", "Sandar Kyaw", "Update C# Learning");
+//adoDotNetExample.Delete(1002);
+//adoDotNetExample.Edit(1002);
+adoDotNetExample.Edit(1);
+
+Console.ReadKey();
