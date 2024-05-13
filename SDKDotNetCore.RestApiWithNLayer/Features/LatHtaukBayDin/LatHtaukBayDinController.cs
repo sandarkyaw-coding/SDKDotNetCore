2e0a6e03050131e3092d4e45f3bb2fe055da74b1
Sandar Kyaw <dars7703@gmail.com>
2024-05-13 09:56:37 +0630

    [Complete]LatHtaukBayDin_JsonData_to_Api

---
+++ SDKDotNetCore.RestApiWithNLayer/Features/LatHtaukBayDin/LatHtaukBayDinController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace SDKDotNetCore.RestApiWithNLayer.Features.LatHtaukBayDin
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LatHtaukBayDinController : ControllerBase
+    {
+        private async Task<LatHtaukBayDin> getDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
+            var model = JsonConvert.DeserializeObject<LatHtaukBayDin>(jsonStr);
+            return model;
+        }
+        // api/LatHaukBayDin/questions
+        [HttpGet("questions")]
+        public async Task<IActionResult> Questions()
+        {
+            var model = await getDataAsync();
+            return Ok(model.questions);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> NumberList()
+        {
+            var model = await getDataAsync();
+            return Ok(model.numberList);
+        }
+
+        [HttpGet("{questionNo}/{no}")]
+        public async Task<IActionResult> Answers(int questionNo, int no)
+        {
+            var model = await getDataAsync();
+            return Ok(model.answers.FirstOrDefault(x=>x.questionNo == questionNo && x.answerNo == no));
+        }
+
+
+        public class LatHtaukBayDin
+        {
+            public Question[] questions { get; set; }
+            public Answer[] answers { get; set; }
+            public string[] numberList { get; set; }
+        }
+
+        public class Question
+        {
+            public int questionNo { get; set; }
+            public string questionName { get; set; }
+        }
+
+        public class Answer
+        {
+            public int questionNo { get; set; }
+            public int answerNo { get; set; }
+            public string answerResult { get; set; }
+        }
+
+    }
+}
