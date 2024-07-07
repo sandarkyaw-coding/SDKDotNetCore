aebe703adbb98216db9f98dfa50927734ea4a1e2
Sandar Kyaw <dars7703@gmail.com>
2024-07-07 18:38:54 +0630

    [Complete]ApexChart_BarWithMarker_MVC

---
+++ SDKDotNetCore.MvcChartApp/Controllers/BarWithMarkerController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SDKDotNetCore.MvcChartApp.Models;
+
+namespace SDKDotNetCore.MvcChartApp.Controllers
+{
+    public class BarWithMarkerController : Controller
+    {
+        public IActionResult BarWithMarker()
+        {
+            BarWithMarkerModel model = new BarWithMarkerModel
+            {
+                Name = "Actual",
+                Expect = new List<ExpecxtList>
+                {
+                    new ExpecxtList
+                    {
+                        X = "2011",
+                        Y = 12,
+                        Goals = new List<GoalsList>
+                        {
+                            new GoalsList
+                            {
+                                Name = "Expected",
+                                Value = 14,
+                                StrokeWidth = 2,
+                                StrokeDashArray = "2",
+                                StrokeColor = "#775DD0"
+                            }
+                        }
+                    },
+                    new ExpecxtList
+                    {
+                        X = "2012",
+                        Y = 44,
+                        Goals = new List<GoalsList>
+                        {
+                            new GoalsList
+                            {
+                                Name = "Expected",
+                                Value = 54,
+                                StrokeWidth = 5,
+                                StrokeHeight = 10,
+                                StrokeColor = "#775DD0"
+                            }
+                        }
+                    },
+                    new ExpecxtList
+                    {
+                        X = "2013",
+                        Y = 23,
+                        Goals = new List<GoalsList>
+                        {
+                            new GoalsList
+                            {
+                                Name = "Expected",
+                                Value = 13,
+                                StrokeWidth = 5,
+                                StrokeHeight = 10,
+                                StrokeColor = "#775DD0"
+                            }
+                        }
+                    },
+                    new ExpecxtList
+                    {
+                        X = "2014",
+                        Y = 34,
+                        Goals = new List<GoalsList>
+                        {
+                            new GoalsList
+                            {
+                                Name = "Expected",
+                                Value = 34,
+                                StrokeWidth = 5,
+                                StrokeHeight = 10,
+                                StrokeColor = "#775DD0"
+                            }
+                        }
+                    },
+                    new ExpecxtList
+                    {
+                        X = "2015",
+                        Y = 81,
+                        Goals = new List<GoalsList>
+                        {
+                            new GoalsList
+                            {
+                                Name = "Expected",
+                                Value = 66,
+                                StrokeWidth = 5,
+                                StrokeHeight = 10,
+                                StrokeColor = "#775DD0"
+                            }
+                        }
+                    },
+                    new ExpecxtList
+                    {
+                        X = "2016",
+                        Y = 67,
+                        Goals = new List<GoalsList>
+                        {
+                            new GoalsList
+                            {
+                                Name = "Expected",
+                                Value = 70,
+                                StrokeWidth = 5,
+                                StrokeHeight = 10,
+                                StrokeColor = "#775DD0"
+                            }
+                        }
+                    }
+                }
+            };
+            return View(model);
+        }
+    }
+}
---
+++ SDKDotNetCore.MvcChartApp/Models/BarWithMarkerModel.cs
@@ -0,0 +1,55 @@
+﻿namespace SDKDotNetCore.MvcChartApp.Models
+{
+    /*   public class BarWithMarkerModel
+       {
+           public string Name { get; set; }
+           public int Value { get; set; }
+           public int StrokeWidth { get; set; }
+           public int StrokeHeight { get; set; }
+           public string StrokeDashArray { get; set; }
+           public string StrokeLineCap { get; set; }
+           public string StrokeColor { get; set; }
+       }
+
+       public class DataSet {
+           public string X { get; set; }
+           public int Y { get; set; }
+           public List<BarWithMarkerModel> Goals { get; set; }
+       }
+
+       public class Series
+       {
+           public string Name { get; set; }
+           public List<DataSet> Data { get; set; }
+       }*/
+
+    public class GoalsList
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public int StrokeWidth { get; set; }
+        public int StrokeHeight { get; set; }
+        public string StrokeDashArray { get; set; }
+        public string StrokeLineCap { get; set; }
+        public string StrokeColor { get; set; }
+    }
+
+    public class ExpecxtList
+    {
+        public string X { get; set; }
+        public int Y { get; set; }
+        public List<GoalsList> Goals { get; set; }
+    }
+
+
+    public class BarWithMarkerModel
+    {
+        public string Name { get; set; }
+        public List<ExpecxtList> Expect { get; set; }
+    }
+
+}
+
+
+
+
