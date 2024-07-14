77d0fe7e57be14d06b15604374814f4606177ff5
Sandar Kyaw <dars7703@gmail.com>
2024-07-14 09:58:41 +0630

    [Complete]HighCharts

---
+++ SDKDotNetCore.MvcChartApp/Controllers/HighChartsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SDKDotNetCore.MvcChartApp.Controllers
+{
+    public class HighChartsController : Controller
+    {
+        public IActionResult PieChart()
+        {
+            return View();
+        }
+    }
+}
