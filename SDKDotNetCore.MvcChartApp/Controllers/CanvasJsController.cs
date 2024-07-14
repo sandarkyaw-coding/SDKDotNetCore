349120ad373b4b848f851fd7080cc23599b6b92e
Sandar Kyaw <dars7703@gmail.com>
2024-07-14 10:05:03 +0630

    [Complete]CanvasJs

---
+++ SDKDotNetCore.MvcChartApp/Controllers/CanvasJsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SDKDotNetCore.MvcChartApp.Controllers
+{
+    public class CanvasJsController : Controller
+    {
+        public IActionResult LineChart()
+        {
+            return View();
+        }
+    }
+}
