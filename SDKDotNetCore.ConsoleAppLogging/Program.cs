dd552ddecc337e09b36cdcc1efe2d2322b4aa564
Sandar Kyaw <dars7703@gmail.com>
2024-07-14 12:39:18 +0630

    [Complete]Console App, ASP.Net Test Log

---
+++ SDKDotNetCore.ConsoleAppLogging/Program.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+			.MinimumLevel.Information()
+			.WriteTo.Console()
+			.WriteTo.File("logs/SDKDotNetCore.ConsoleAppLogging.txt", rollingInterval: RollingInterval.Hour)
+			.CreateLogger();
+
+Log.Fatal("Hello, world!");
+Log.Information("Hello, world!");
+
+int a = 10, b = 0;
+try
+{
+	Log.Debug("Dividing {A} by {B}", a, b);
+	Console.WriteLine(a / b);
+}
+catch (Exception ex)
+{
+	Log.Error(ex, "Something went wrong");
+}
+finally
+{
+	await Log.CloseAndFlushAsync();
+}
+   
--- SDKDotNetCore.MvcChartApp/Models/BarWithMarkerModel.cs
+++ SDKDotNetCore.MvcChartApp/Models/BarWithMarkerModel.cs
@@ -1,28 +1,5 @@
 ﻿namespace SDKDotNetCore.MvcChartApp.Models
 {
-    /*   public class BarWithMarkerModel
-       {
-           public string Name { get; set; }
-           public int Value { get; set; }
-           public int StrokeWidth { get; set; }
-           public int StrokeHeight { get; set; }
-           public string StrokeDashArray { get; set; }
-           public string StrokeLineCap { get; set; }
-           public string StrokeColor { get; set; }
-       }
-
-       public class DataSet {
-           public string X { get; set; }
-           public int Y { get; set; }
-           public List<BarWithMarkerModel> Goals { get; set; }
-       }
-
-       public class Series
-       {
-           public string Name { get; set; }
-           public List<DataSet> Data { get; set; }
-       }*/
-
     public class GoalsList
     {
         public string Name { get; set; }
