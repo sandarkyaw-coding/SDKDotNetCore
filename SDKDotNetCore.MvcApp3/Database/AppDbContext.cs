bbf994f13d84ab0824c874fe5f5b8e3672e99c5e
Sandar Kyaw <dars7703@gmail.com>
2024-10-17 11:38:30 +0630

    [Complete]58_MVC Ajax(Blog)

---
+++ SDKDotNetCore.MvcApp3/Database/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace SDKDotNetCore.MvcApp3.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<BlogEntity> Blogs { get; set; }
+    }
+
+    //Data Model
+    [Table("Tbl_Blog")]
+    public class BlogEntity
+    {
+        [Key]
+        public int BlogId { get; set; }
+
+        public string BlogTitle { get; set; }
+
+        public string BlogAuthor { get; set; }
+        
+        public string  BlogContent { get; set; }
+    }
+}
